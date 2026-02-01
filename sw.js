066f2a83272bb1ee5a94bd75f23657f35ca91dc7
Flavio Cardoso <makumba@MakumbaMBP.home>
2026-02-01 14:58:49 +0100

    Guia de Compras 2026 – PWA

---
+++ sw.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('gc-v2').then(c => c.addAll(['./'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
