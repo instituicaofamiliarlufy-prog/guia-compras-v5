1cd5db1586d71d2479b5b0935d0e547a6f08b483
Flavio Cardoso <makumba@Mac.home>
2026-02-08 00:33:40 +0100

    added firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGzgFbleV3H6tZpOEW0voPke0LY9VTJs8",
+  authDomain: "guia-de-compras-2f883.firebaseapp.com",
+  projectId: "guia-de-compras-2f883",
+  storageBucket: "guia-de-compras-2f883.firebasestorage.app",
+  messagingSenderId: "375038437557",
+  appId: "1:375038437557:web:f3e21dc6f40b2a04e076fc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
