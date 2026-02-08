daeb5c662014cb7de06b910ead0c2d4f0a4f5c29
instituicaofamiliarlufy-prog <instituicaofamiliarlufy@gmail.com>
2026-02-08 18:42:53 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
